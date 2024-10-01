a0b547f8eccbc2ced774e6d6e7a45014f4150a57
FelipeAP04 <agu23195@uvg.edu.gt>
2024-09-30 22:01:21 -0600

    Comentarios ejercicio 1

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,3 +1,20 @@
+/*
+ * UNIVERSIDAD DEL VALLE DE GUATEMALA
+ * CC3086 - Programacion de Microprocesadores
+ * Ver.: 1.1 2024/21/08
+ * Mod.: Jorge Luis Felipe Aguilar Portillo
+ * Mod.: Angie Nadissa Vela López
+ * Carnet: 23764
+ *
+ * References:
+ * Rauber, T., & Rünger, G. (2023). Parallel programming:
+ * For multicore and cluster systems (3a ed.). Springer International Publishing.
+ * -------------------------------------------------------------
+ * Programa que simula el funcionamiento de una fábrica
+ * de producción de café tostado molido
+ * -------------------------------------------------------------
+ */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -5,12 +22,14 @@
 #define NUM_THREADS 8
 #define NUM_PRODUCTS 8
 
+//declaramos valores y nombres de productos
 char productos[][30] = {"Porcion pastel de chocolate: ", "White mocha: ", "Cafe americano 8oz:", "Latte 8oz:", "Toffee coffee: ", "Cappuccino 8oz: ", "S'mores latte: ", "Cafe tostado molido: "};
 float unitario[] = {60.00, 32.00, 22.00, 24.00, 28.00, 24.00, 32.00, 60.00};
 float fijo[] = {20.00, 19.20, 13.20, 17.20, 20.10, 17.20, 23.00, 20.00};
 int vendidosJulio[] = {300, 400, 1590, 200, 390, 1455, 800, 60};
 int vendidosAgosto[] = {250, 380, 800, 250, 600, 1200, 1540, 15};
 
+//arrays finales en los que se guardaran los varores finales
 float totVentasJulio[NUM_PRODUCTS];
 float totUtilidadesJulio[NUM_PRODUCTS];
 float totVentasAgosto[NUM_PRODUCTS];
@@ -18,10 +37,12 @@ float totUtilidadesAgosto[NUM_PRODUCTS];
 
 pthread_mutex_t mutex;
 
+// Función para calcular el total de ventas de cada producto en julio y agosto 
 void *ventasProducto(void *arg) {
-    int id = *(int *)arg;
+    int id = *(int *)arg; //que hilo hace el proceso
     printf("calculo hilo %d iniciado\n", id);
     sleep(1); 
+    //por calcula por cada producto, calcula julio y agosto
     for (int i = 0; i < NUM_PRODUCTS; i++) {
         totVentasJulio[i] = vendidosJulio[i] * unitario[i];
         totVentasAgosto[i] = vendidosAgosto[i] * unitario[i];
@@ -30,10 +51,12 @@ void *ventasProducto(void *arg) {
     return NULL;
 }
 
+// Función para calcular la utilidad de cada producto en julio y agosto
 void *utilidadProducto(void *arg) {
-    int id = *(int *)arg;
+    int id = *(int *)arg;//que hilo hace el proceso
     printf("calculo hilo %d iniciado\n", id);
     sleep(1); 
+    // Calculamos la utilidad con formula 
     for (int i = 0; i < NUM_PRODUCTS; i++) {
         totUtilidadesJulio[i] = totVentasJulio[i] - (vendidosJulio[i] * fijo[i]);
         totUtilidadesAgosto[i] = totVentasAgosto[i] - (vendidosAgosto[i] * fijo[i]);
@@ -58,18 +81,19 @@ void *totUtilidades(void *arg) {
     return NULL;
 }
 
+// Función para generar un reporte final con las ventas y utilidades de cada mes
 void reporteFinal(char *mes, float *ventas, float *utilidades) {
     printf("------------------------------------------\n");
     printf("REPORTE DEL MES DE %s\n", mes);
-    printf("--- Monto Ventas por Producto ---\n");
+    printf("--- Monto Ventas por Producto ---\n"); // Imprimimos el monto de ventas por producto
     for (int i = 0; i < NUM_PRODUCTS; i++) {
         printf("%s Q%.0f\n", productos[i], ventas[i]);
     }
-    printf("--- Utilidad por Producto ---\n");
+    printf("--- Utilidad por Producto ---\n"); // Imprimimos la utilidad por producto
     for (int i = 0; i < NUM_PRODUCTS; i++) {
         printf("%s Q%.0f\n", productos[i], utilidades[i]);
     }
-    float totalVentas = 0;
+    float totalVentas = 0; // Calculamos el total de ventas y utilidades del mes
     float totalUtilidades = 0;
     for (int i = 0; i < NUM_PRODUCTS; i++) {
         totalVentas += ventas[i];
